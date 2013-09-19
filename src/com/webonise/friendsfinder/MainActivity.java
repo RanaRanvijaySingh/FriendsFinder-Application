fdd0413c2199baead25286025944d18037c0d573
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2013-09-19 19:25:20 +0530

    map and frinds list fragment added

--- src/com/webonise/friendsfinder/MainActivity.java
+++ src/com/webonise/friendsfinder/MainActivity.java
@@ -1,10 +1,10 @@
 package com.webonise.friendsfinder;
 
-import com.example.friendsfinder.R;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
+
+import com.example.friendsfinder.R;
 
 public class MainActivity extends FragmentActivity {
 	private FragmentTabHost mTabHost;
@@ -14,20 +14,27 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+		setTabContent();
+
+	}
+
+	private void setTabContent() {
 		mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
 		mTabHost.setup(this, getSupportFragmentManager(), R.id.tabFrameLayout);
 
+		
 		mTabHost.addTab(
-				mTabHost.newTabSpec("tab1").setIndicator("Tab 1",
+				mTabHost.newTabSpec("map").setIndicator("Map",
 						getResources().getDrawable(android.R.drawable.star_on)),
-				FragmentTab.class, null);
+				MapFragment.class, null);
 		mTabHost.addTab(
-				mTabHost.newTabSpec("tab2").setIndicator("Tab 2",
+				mTabHost.newTabSpec("friends list").setIndicator("Friends List",
 						getResources().getDrawable(android.R.drawable.star_on)),
-				FragmentTab.class, null);
+				FriendsListFragment.class, null);
 		mTabHost.addTab(
 				mTabHost.newTabSpec("tab3").setIndicator("Tab 3",
 						getResources().getDrawable(android.R.drawable.star_on)),
 				FragmentTab.class, null);
+
 	}
 }
