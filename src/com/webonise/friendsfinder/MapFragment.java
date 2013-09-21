cb8dbd5cfbda87bff75e71e6b60ee2651d7138c9
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2013-09-21 16:56:34 +0530

    saturday's work

--- src/com/webonise/friendsfinder/MapFragment.java
+++ src/com/webonise/friendsfinder/MapFragment.java
@@ -2,9 +2,11 @@ package com.webonise.friendsfinder;
 
 
 import com.example.friendsfinder.R;
+import com.webonise.friendsfinder.helper.MapFragmentActivity;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +16,8 @@ public class MapFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view=inflater.inflate(R.layout.google_map, container,false);
-		
+		MapFragmentActivity mapFragmentActivity=new MapFragmentActivity();
+		mapFragmentActivity.initializeMap();
 		return view;
 
 	}
---
+++ src/com/webonise/friendsfinder/helper/MapFragmentActivity.java
@@ -0,0 +1,21 @@
+package com.webonise.friendsfinder.helper;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+public class MapFragmentActivity extends FragmentActivity
+{
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		
+	}
+	public void initializeMap()
+	{
+		Log.i(null,"in here");
+	}
+
+}
