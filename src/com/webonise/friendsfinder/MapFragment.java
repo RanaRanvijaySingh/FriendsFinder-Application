a80c75123898c4577422f6ee3646d4f2ee0d5522
ranvijaySingh-Webonise <rana.ranvijay@weboniselab.com>
2013-09-21 17:13:22 +0530

    dynamic fragment added for map

--- src/com/webonise/friendsfinder/MapFragment.java
+++ src/com/webonise/friendsfinder/MapFragment.java
@@ -1,25 +1,38 @@
 package com.webonise.friendsfinder;
 
 
-import com.example.friendsfinder.R;
-import com.webonise.friendsfinder.helper.MapFragmentActivity;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.friendsfinder.R;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MapFragment extends Fragment {
+	MainActivity mainActivity;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view=inflater.inflate(R.layout.google_map, container,false);
-		MapFragmentActivity mapFragmentActivity=new MapFragmentActivity();
-		mapFragmentActivity.initializeMap();
+		mainActivity=(MainActivity)getActivity();
+		initializeMap();
 		return view;
 
 	}
-
+	private void initializeMap() {
+		Object googleMap = null;
+		if(googleMap==null)
+		{
+			SupportMapFragment supportMapFragment = ((SupportMapFragment)mainActivity.getSupportFragmentManager().findFragmentById(R.id.map));
+			googleMap= supportMapFragment.getMap();
+			
+			if(googleMap== null ) {
+				Toast.makeText(mainActivity, "Sorry the map is not initialize", Toast.LENGTH_LONG).show();
+			}
+		}
+		
+	}
 }
